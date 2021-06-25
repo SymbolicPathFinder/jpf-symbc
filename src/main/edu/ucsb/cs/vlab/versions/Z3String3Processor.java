e32a4384425172b140c7c4bf7c2324cbc9d62bb5
Marlin Roberts <marlinroberts@u.boisestate.edu>
2021-06-25 14:44:28 -0600

    z3String3Processor cleanup

--- src/main/edu/ucsb/cs/vlab/versions/Z3String3Processor.java
+++ src/main/edu/ucsb/cs/vlab/versions/Z3String3Processor.java
@@ -15,6 +15,8 @@ import edu.ucsb.cs.vlab.Z3Interface.Processor;
 import edu.ucsb.cs.vlab.modelling.Output;
 import edu.ucsb.cs.vlab.modelling.Output.Model;
 
+import com.microsoft.z3.*;
+
 public class Z3String3Processor implements Processable {
 	final Model model = new Model();
 	final StringBuilder currentQuery = new StringBuilder();
@@ -39,19 +41,14 @@ public class Z3String3Processor implements Processable {
 		try (final BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
 			String line = reader.readLine();
 
-			// MJR
-//			if (!line.startsWith("*")) {
-//				throw new ExternalToolException(line);
-//			}
-
-			// MJR
-			//reader.readLine();
-			// line = reader.readLine();
-			//System.out.println("line looks like.........:" + line);
 			sat = line.replace(">> ", "").trim().equalsIgnoreCase("SAT");
-			//reader.readLine();
 
-			// MJR
+			
+			
+			
+			
+			
+			
 			List<String> solutions = new ArrayList<>();
 			if (sat) {
 				while (!reader.ready()) {}
@@ -60,7 +57,6 @@ public class Z3String3Processor implements Processable {
 					if (line.contains("define-fun")) {
 						solutions.add(line + reader.readLine());
 					}
-					//System.out.println(line);
 				}
 			}
 
@@ -69,24 +65,12 @@ public class Z3String3Processor implements Processable {
 				System.out.println(s.trim());
 				String value = s.substring(s.indexOf("\""), s.length() -1);
 				String[] parts = s.split(" ");
-//				for (String st : parts) {
-//					System.out.println(st);
-//				}
+
 				String processString = parts[3] + " : " + parts[5] + " -> ";
-				//String value = parts[9].substring(0, parts[9].length()-1);
 				processString = processString + value;
-				//System.out.println(processString);
 				process(processString);
 
 			}
-			// MJR
-//			if (sat) {
-//				while (!(line = reader.readLine()).startsWith("*")) {
-//					process(line);
-//				}
-//			}
-
-
 		}
 
 		return new Output(sat, assembleModel());
