cfbe68441e4f0aa329db694382d9b53ad4e682a1
kylekim72 <kunha9912@gmail.com>
2024-08-25 22:06:29 +0900

    Add comments for object

--- jpf-symbc/src/main/gov/nasa/jpf/symbc/witness/SymbolicVariableInfo.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/witness/SymbolicVariableInfo.java
@@ -1,5 +1,12 @@
-package gov.nasa.jpf.symbc.witness;
+/**
+ * Object that contains the required information to construct violation witness
+ * Variable lineNumber denotes the line number of symbolic variable(Verifier.nondet~()'s invocation)
+ * Variable returnType denotes the type of symbolic variable
+ * Variable varName denotes the name of symbolic variable
+ * Variable varValue denotes the value of symbolic variable
+ */
 
+package gov.nasa.jpf.symbc.witness;
 
 
 public class SymbolicVariableInfo{
