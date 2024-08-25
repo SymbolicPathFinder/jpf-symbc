1e91da896227a94b6a84e3438daf5d18b4dc971b
kylekim72 <kunha9912@gmail.com>
2024-08-25 22:11:09 +0900

    Add comments for object and methods

--- jpf-symbc/src/main/gov/nasa/jpf/symbc/witness/PathConditionParser.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/witness/PathConditionParser.java
@@ -1,3 +1,7 @@
+/**
+ * Object for parsing symbolic variable at PathCondition
+ * It has one method, parseSymVar()
+ */
 package gov.nasa.jpf.symbc.witness;
 
 
@@ -6,9 +10,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PathConditionParser{
-
+    /**
+     * It parses a value of symbolic variable from PathCondition, and match it to corresponding variable
+     * @param strPathCondition is a String type PathCondition
+     * @param symbolicVariableInfoList is a list that contains the information of symbolic variables
+     */
     public void parseSymVar(String strPathCondition, List<SymbolicVariableInfo> symbolicVariableInfoList){
-        // Currently not handling String type
         // Extract variable name and value
         Pattern pattern = Pattern.compile("(\\w+)\\[(-?\\d+(\\.\\d+)?([eE][-+]?\\d+)?)\\]");
         Matcher matcher = pattern.matcher(strPathCondition);
@@ -19,6 +26,8 @@ public class PathConditionParser{
             String pcVariableName = matcher.group(1);
             String pcVariableValue = matcher.group(2);
             // There are 3 cases, real number, string and other numeric types
+
+            // For real type
             if (pcVariableName.contains("double") || pcVariableName.contains("float") || pcVariableName.contains("REAL")) {
                 double value = Double.parseDouble(pcVariableValue); // 실수로 파싱
                 for(int i=0; i<symbolicVariableInfoList.size(); i++){
@@ -29,7 +38,7 @@ public class PathConditionParser{
                 }
 
             }
-
+            // For String type
             else if (pcVariableName.contains("string")) {
                 for (int i = 0; i < symbolicVariableInfoList.size(); i++) {
                     if (symbolicVariableInfoList.get(i).varName.equals(pcVariableName)) {
@@ -39,12 +48,12 @@ public class PathConditionParser{
                 }
 
             }
-
+            // Other types
             else {
                 int value = Integer.parseInt(pcVariableValue);
                 for(int i=0; i<symbolicVariableInfoList.size(); i++){
                     if(symbolicVariableInfoList.get(i).varName.equals(pcVariableName)){
-                        // SPF outputs true or false as 1 or 0, respectively. Handle such cases
+                        // special case for boolean type
                         if(symbolicVariableInfoList.get(i).returnType.equals("boolean")){
                             if(value == 1) symbolicVariableInfoList.get(i).varValue = true;
                             else if(value == 0) symbolicVariableInfoList.get(i).varValue = false;
