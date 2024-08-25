89bf697e59612239cddc119a7113879d6fa81e3b
kylekim72 <kunha9912@gmail.com>
2024-08-25 21:46:11 +0900

    Add comments for object and methods

--- jpf-symbc/src/main/gov/nasa/jpf/symbc/witness/Node.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/witness/Node.java
@@ -1,3 +1,11 @@
+/**
+ * Object that represents the node part of violation witness
+ * It has two methods, serializeNode() and serializeAllNodes()
+ * Variable numberOfNode denotes the total number of node of violation witness
+ * Variable indexOfNode denotes the index of the node
+ * Variable noCounterExample is a flag that shows whether the counterexample is exists or not
+ */
+
 package gov.nasa.jpf.symbc.witness;
 
 import java.util.List;
@@ -9,6 +17,13 @@ public class Node{
     public int indexOfNode;
     public boolean noCounterExample;
 
+    /**
+     * It serializes a single node of violation witness
+     * If there is no counterexample or the number of node is zero, it generates the node
+     * that contains two keys, entry and violation
+     * Else, it generates the node as String
+     * @return A string that represents single node of violation witness
+     */
     public String serializeNode(){
         // StringBuilder for serializing node of witness
         StringBuilder nodeBuilder = new StringBuilder(System.lineSeparator());
@@ -31,6 +46,11 @@ public class Node{
         return nodeBuilder.toString();
     }
 
+    /**
+     * It serializes every node in nodeList
+     * @param nodeList contains all nodes of the violation witness
+     * @return A string that represents every node of the violation witness
+     */
     public static String serializeAllNodes(List<Node> nodeList){
         StringBuilder allNodesBuilder = new StringBuilder();
         for(Node node : nodeList){
