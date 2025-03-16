fadbd178e2b003b2392cc815c1734e74918be3a9
saifk16 <saif.khan16@outlook.com>
2025-03-16 14:40:18 +0530

    Handle NaN and -0.0f/-0.0d max/min

--- jpf-symbc/src/classes/java/lang/Math.java
+++ jpf-symbc/src/classes/java/lang/Math.java
@@ -38,7 +38,7 @@
 package java.lang;
 
 public class Math {
-	public static final double PI = 3.141592653589793;
+	public static final double PI = 3.14159265358979323846;
 
 	public static double abs ( double a) {
 	    return (a < 0.0) ? -a : a;
@@ -58,11 +58,19 @@ public class Math {
 
       //	 TODO:
 	  public static double max ( double a, double b) {
+		if (!(a == a))
+			return a;
+		if (a == b)
+			return (1.0d / a > 0) ? a : b;
 		return (a >= b) ? a : b;
 	  }
 
 	  // TODO: need to model NaN et al.
 	  public static float max ( float a, float b) {
+		if (!(a == a))
+			return a;
+		if (a == b)
+			return (1.0f / a > 0) ? a : b;
 		return (a >= b) ? a : b;
 	  }
 
@@ -76,11 +84,19 @@ public class Math {
 
 	  // TODO:
 	  public static double min ( double a, double b) {
-		return (a >= b) ? a : b;
+		if (!(a == a))
+			return a;
+		if (a == b)
+			return (1.0d / a > 0) ? a : b;
+		return (a <= b) ? a : b;
 	  }
 
 	  // TODO:
 	  public static float min ( float a, float b) {
+		 if (!(a == a))
+			 return a;
+		 if (a == b)
+			 return (1.0f / a > 0) ? a : b;
 		 return (a <= b) ? a : b;
 	  }
 
