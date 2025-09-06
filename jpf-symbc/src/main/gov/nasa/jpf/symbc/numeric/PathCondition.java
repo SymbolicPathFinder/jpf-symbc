5f11f1bff6c41510f0626d91bcbfed588a46125b
RehanChalana <rehanchalanaprsnl@gmail.com>
2025-09-06 19:15:17 +0530

    Revert "chore: add ParallelTest1 example"
    This reverts commit 697bdc66a7539b1a676ce19b4569d33c7d4c45c7.

--- jpf-symbc/src/main/gov/nasa/jpf/symbc/numeric/PathCondition.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/numeric/PathCondition.java
@@ -393,6 +393,7 @@ public class PathCondition implements Comparable<PathCondition> {
             result1 = pa.isSatisfiable(this, solver);
         } else
             result1 = solver.isSatisfiable(this);
+
         solverCalls++;
         solver.cleanup();
 
