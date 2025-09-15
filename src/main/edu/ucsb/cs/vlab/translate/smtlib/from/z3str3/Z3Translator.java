945013795c3063987acfb8c382030854ff6a47cf
natsteven <nathanaelsteven@u.boisestate.edu>
2025-09-15 15:27:58 -0600

    delete, insert, reverse, case support

--- src/main/edu/ucsb/cs/vlab/translate/smtlib/from/z3str3/Z3Translator.java
+++ src/main/edu/ucsb/cs/vlab/translate/smtlib/from/z3str3/Z3Translator.java
@@ -101,20 +101,18 @@ class Manager extends TranslationManager {
 				Operator op = ((BinaryLinearIntegerExpression) x).getOp();
 				IntegerExpression left = ((BinaryLinearIntegerExpression) x).getLeft();
 				IntegerExpression right = ((BinaryLinearIntegerExpression) x).getRight();
-				if(op.name().equals("AND")){
-					if(left instanceof SymbolicCharAtInteger
+				if (op.name().equals("AND")) {
+					if (left instanceof SymbolicCharAtInteger
 							&& right instanceof IntegerConstant
-							&& ((IntegerConstant)right).value() == 65535){
+							&& ((IntegerConstant) right).value() == 65535) {
 						return evaluateExpression(SymbolicCharAtInteger.class, left, "str.at $getExpression %getIndex");
-					}
-					else if(right instanceof SymbolicCharAtInteger
+					} else if (right instanceof SymbolicCharAtInteger
 							&& left instanceof IntegerConstant
-							&& ((IntegerConstant)left).value() == 65535){
+							&& ((IntegerConstant) left).value() == 65535) {
 						return evaluateExpression(SymbolicCharAtInteger.class, right, "str.at $getExpression %getIndex");
-					}
-					else
+					} else
 						return evaluateExpression(BinaryLinearIntegerExpression.class, x, "str.from_code ( bv2int ( _getOp (( _ int2bv 32) ( str.to_code %getLeft )) (( _ int2bv 32 ) %getRight )))");
-				}else
+				} else
 					return evaluateExpression(BinaryLinearIntegerExpression.class, x, "_getOp %getLeft %getRight");
 			});
 		}
@@ -131,7 +129,7 @@ class Manager extends TranslationManager {
 		public void init() {
 			final Function<StringConstraint, String> IsInteger = (x) -> {
 				final String in_str = manager.strExpr.collect((StringExpression) x.getRight());
-				return "(or (not (= (str.to_int " + in_str + ") -1)) (and (= (str.at "+ in_str +" 0) \"-\") (not (= (str.to_int (str.substr " + in_str + " 1 (str.len " + in_str + "))) -1))))";
+				return "(or (not (= (str.to_int " + in_str + ") -1)) (and (= (str.at " + in_str + " 0) \"-\") (not (= (str.to_int (str.substr " + in_str + " 1 (str.len " + in_str + "))) -1))))";
 			};
 
 			final Function<String, Function<StringConstraint, String>> RightLeftTemplate = (prefix) -> {
@@ -223,10 +221,10 @@ class Manager extends TranslationManager {
 			};
 
 			final Function<StringExpression, String> ValueOfInt = (expr) -> {
-					final DerivedStringExpression dse = (DerivedStringExpression) expr;
-					final String arg = manager.numExpr.collect((IntegerExpression) dse.oprlist[0]);
-					return "(ite ( < " + arg + " 0) (str.++ \"-\" (str.from_int (- " + arg + "))) (str.from_int " +  arg + "))";
-				};
+				final DerivedStringExpression dse = (DerivedStringExpression) expr;
+				final String arg = manager.numExpr.collect((IntegerExpression) dse.oprlist[0]);
+				return "(ite ( < " + arg + " 0) (str.++ \"-\" (str.from_int (- " + arg + "))) (str.from_int " + arg + "))";
+			};
 
 			map(StringOrOperation.NONSYM, (expr) -> {
 				return "\"" + ((StringConstant) expr).value + "\"";
@@ -274,8 +272,50 @@ class Manager extends TranslationManager {
 				Results.constraints.add("(assert (and (str.in_re " + in_str + " (re.++ ws (str.to_re out_str) ws)) (or (= out_str \"\")(and (str.in_re out_str (re.++ nwc re.all)) (str.in_re out_str (re.++ re.all nwc))))))");
 				return arg;
 			});
-			map(StringOrOperation.TOLOWERCASE, RightTemplate.apply("(toLowerCase"));
-			map(StringOrOperation.TOUPPERCASE, RightTemplate.apply("(toUpperCase"));
+			map(StringOrOperation.TOLOWERCASE, (expr) -> {
+				final DerivedStringExpression dse = (DerivedStringExpression) expr;
+				final String in_str = manager.strExpr.collect(dse.right);
+				String arg = "out_str";
+				Results.stringVariables.add("out_str");
+				Results.constraints.add(
+						"(define-fun-rec toLower ((x String) (y String)) Bool " +
+							"(or (and (= x \"\") (= y \"\")) " +
+								"(and (not (= x \"\")) (not (= y \"\")) " +
+									"(let ((x_head (str.at x 0)) " +
+										"(y_head (str.at y 0)) " +
+										"(x_tail (str.substr x 1 (- (str.len x) 1))) " +
+										"(y_tail (str.substr y 1 (- (str.len y) 1)))) " +
+									"(and (= (str.to_code y_head) " +
+										"(ite (and (<= 65 (str.to_code x_head)) (<= (str.to_code x_head) 90)) " +
+											"(+ (str.to_code x_head) 32) " +
+											"(str.to_code x_head))) " +
+										"(toLower x_tail y_tail))))))" +
+						"\n(assert (toLower " + in_str + " out_str))"
+				);
+				return arg;
+			});
+			map(StringOrOperation.TOUPPERCASE, (expr) -> {
+				final DerivedStringExpression dse = (DerivedStringExpression) expr;
+				final String in_str = manager.strExpr.collect(dse.right);
+				String arg = "out_str";
+				Results.stringVariables.add("out_str");
+				Results.constraints.add(
+				    "(define-fun-rec toUpper ((x String) (y String)) Bool " +
+				        "(or (and (= x \"\") (= y \"\")) " +
+				            "(and (not (= x \"\")) (not (= y \"\")) " +
+				                "(let ((x_head (str.at x 0)) " +
+				                    "(y_head (str.at y 0)) " +
+				                    "(x_tail (str.substr x 1 (- (str.len x) 1))) " +
+				                    "(y_tail (str.substr y 1 (- (str.len y) 1)))) " +
+				                "(and (= (str.to_code y_head) " +
+				                    "(ite (and (<= 97 (str.to_code x_head)) (<= (str.to_code x_head) 122)) " +
+				                        "(- (str.to_code x_head) 32) " +
+				                        "(str.to_code x_head))) " +
+				                    "(toUpper x_tail y_tail))))))" +
+				    "\n(assert (toUpper " + in_str + " out_str))"
+				);
+				return arg;
+			});
 
 			map(StringOrOperation.VALUEOF, (expr) -> {
 				String arg = null;
@@ -284,24 +324,20 @@ class Manager extends TranslationManager {
 					arg = manager.strExpr.collect((StringExpression) dse.oprlist[0]);
 				} else if (dse.oprlist[0] instanceof IntegerExpression) {
 					if ((dse.oprlist[0] instanceof SymbolicInteger) && !(dse.oprlist[0] instanceof SymbolicCharAtInteger)) {
-						SymbolicInteger op = (SymbolicInteger)dse.oprlist[0];
-						if(op._min == 0 && op._max == 65535)
+						SymbolicInteger op = (SymbolicInteger) dse.oprlist[0];
+						if (op._min == 0 && op._max == 65535)
 							arg = "(str.from_code " + manager.numExpr.collect((IntegerExpression) dse.oprlist[0]) + ")";
 						else
 							arg = ValueOfInt.apply(expr);
-					}
-					else if(dse.oprlist[0] instanceof SymbolicCharAtInteger){
+					} else if (dse.oprlist[0] instanceof SymbolicCharAtInteger) {
 						arg = manager.numExpr.collect((IntegerExpression) dse.oprlist[0]);
-					}
-					else if (dse.oprlist[0] instanceof BinaryLinearIntegerExpression){
-						BinaryLinearIntegerExpression op = (BinaryLinearIntegerExpression)dse.oprlist[0];
-						if(op.getOp().name().equals("AND") && (op.getLeft() instanceof SymbolicCharAtInteger || op.getRight() instanceof SymbolicCharAtInteger)){
+					} else if (dse.oprlist[0] instanceof BinaryLinearIntegerExpression) {
+						BinaryLinearIntegerExpression op = (BinaryLinearIntegerExpression) dse.oprlist[0];
+						if (op.getOp().name().equals("AND") && (op.getLeft() instanceof SymbolicCharAtInteger || op.getRight() instanceof SymbolicCharAtInteger)) {
 							arg = manager.numExpr.collect((IntegerExpression) dse.oprlist[0]);
-						}
-						else
+						} else
 							arg = ValueOfInt.apply(expr);
-					}
-					else
+					} else
 						arg = ValueOfInt.apply(expr);
 				}
 
@@ -311,6 +347,41 @@ class Manager extends TranslationManager {
 				} catch (NumberFormatException e) {
 					return arg;
 				}
+			});
+			map(StringOrOperation.DELETE, (expr) -> {
+				final DerivedStringExpression dse = (DerivedStringExpression) expr;
+				final String in_str = manager.strExpr.collect((StringExpression) dse.oprlist[0]);
+				final String arg1 = manager.numExpr.collect((IntegerExpression) dse.oprlist[1]);
+				final String arg2 = manager.numExpr.collect((IntegerExpression) dse.oprlist[2]);
+				Results.constraints.add("(assert (<= " + arg2 + " (str.len " + in_str + ")))");
+				return "(str.++ (str.substr " + in_str + " 0 " + arg1 + ") (str.substr " + in_str + " " + arg2 + " (str.len " + in_str + ")))";
+			});
+			map(StringOrOperation.INSERT, (expr) -> {
+				final DerivedStringExpression dse = (DerivedStringExpression) expr;
+				final String in_str = manager.strExpr.collect((StringExpression) dse.oprlist[0]);
+				final String insert_str = manager.strExpr.collect((StringExpression) dse.oprlist[1]);
+				final String arg1 = manager.numExpr.collect((IntegerExpression) dse.oprlist[2]);
+				Results.constraints.add("(assert (<= " + arg1 + " (str.len " + in_str + ")))");
+				return "(str.++ (str.substr " + in_str + " 0 " + arg1 + ") " + insert_str + " (str.substr " + in_str + " " + arg1 + " (str.len " + in_str + ")))";
+			});
+			map(StringOrOperation.REVERSE, (expr) -> {
+				final DerivedStringExpression dse = (DerivedStringExpression) expr;
+				final String in_str = manager.strExpr.collect((StringExpression) dse.oprlist[0]);
+				String var = "out_str";
+				Results.stringVariables.add("out_str");
+				Results.constraints.add(
+				    "(define-fun-rec reverse ((x String) (y String)) Bool " +
+				        "(or (and (= x \"\") (= y \"\")) " +
+				            "(and (not (= x \"\")) (not (= y \"\")) " +
+				                "(let ((x_head (str.at x 0)) " +
+				                    "(y_head (str.at y (- (str.len y) 1))) " +
+				                    "(x_tail (str.substr x 1 (- (str.len x) 1))) " +
+				                    "(y_tail (str.substr y 0 (- (str.len y) 1)))) " +
+				                "(and (= x_head y_head) " +
+				                    "(reverse x_tail y_tail))))))" +
+				    "\n(assert (reverse " + in_str + " out_str))"
+				);
+				return var;
 			});
 		}
 	}
